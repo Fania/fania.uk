d4c629fa901dd304be74e22073be2b28e3220981
Fania Ra <fania@web.de>
2018-11-04 20:14:39 +0000

    add pata date

--- meta/scripts.js
+++ meta/scripts.js
@@ -13,3 +13,12 @@ menuup.addEventListener("click", () => {
   menudown.classList.remove("hide");
 });
 
+// let now = new Date();
+let lastEdited = new Date("04 November 2018");
+// let pnow = new PataphysicalDate();
+let plastEdited = new PataphysicalDate(lastEdited);
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+date.innerHTML = plastEdited;
+date.setAttribute("title", `${lastEdited.getDate()} ${months[lastEdited.getMonth()]} ${lastEdited.getFullYear()} vulg.`);
+
