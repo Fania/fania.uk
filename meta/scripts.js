7a9fc924a9fff8d4eb5cdc6276e65fe7bed0d971
Fania Ra <fania@web.de>
2018-11-04 19:25:38 +0000

    icon menu mobile

--- meta/scripts.js
+++ meta/scripts.js
@@ -1,18 +1,15 @@
 // nothing
 
-// menuicon.innerHTML ='<i class="fas fa-angle-down"></i>';
 
-let i = 0;
-const arrows = ['<i class="fas fa-angle-down"></i>',
-                '<i class="fas fa-angle-up"></i>'];
-
-menuicon.addEventListener("click", () => {
-  console.log("test");
+menudown.addEventListener("click", () => {
   menu.classList.toggle("menutoggle");
-  i===0 ? i=1 : i=0;
-  menuicon.innerHTML = arrows[i];
+  menudown.classList.add("hide");
+  menuup.classList.remove("hide");
 });
 
+menuup.addEventListener("click", () => {
+  menu.classList.toggle("menutoggle");
+  menuup.classList.add("hide");
+  menudown.classList.remove("hide");
+});
 
-// "<i class="fas fa-angle-up"></i>"
-// "<i class="fas fa-angle-down"></i>"
