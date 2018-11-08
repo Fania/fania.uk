ded8cf944610bfff02b35d280046450a227b8a6d
Fania Ra <fania@web.de>
2018-11-08 20:19:26 +0000

    scroll to top, hash url

--- meta/scripts.js
+++ meta/scripts.js
@@ -1,17 +1,31 @@
-// nothing
+// LOAD ON CORRECT PAGE
+
+window.addEventListener("load", function(event) {
+  const pages = ["#home", "#uni", "#bookmarks"];
+  const hash = window.location.hash;
+
+  if(pages.includes(hash)) {
+    console.log("yay");
+  }
+
+});
+
+
+
 
 // MOBILE MENU
 menudown.addEventListener("click", () => {
-  menu.classList.toggle("menutoggle");
+  menu.classList.add("showmenu");
   menudown.classList.add("hide");
   menuup.classList.remove("hide");
 });
 
 menuup.addEventListener("click", () => {
-  menu.classList.toggle("menutoggle");
+  menu.classList.remove("showmenu");
   menuup.classList.add("hide");
   menudown.classList.remove("hide");
 });
+
 
 
 // MENU LINKS
@@ -20,41 +34,63 @@ bookmarksM.classList.add("hide"); bookmarksA.classList.add("hide");
 uniM.classList.add("hide"); uniA.classList.add("hide");
 cheatsM.classList.add("hide"); cheatsA.classList.add("hide");
 
+// let loadpage = (button) => {
+//   button.addEventListener("click", () => {
+//     location.replace(`#${button}`);
+//     homeM.classList.remove("hide"); homeA.classList.remove("hide");
+//     bookmarksM.classList.add("hide"); bookmarksA.classList.add("hide");
+//     uniM.classList.add("hide"); uniA.classList.add("hide");
+//     cheatsM.classList.add("hide"); cheatsA.classList.add("hide");
+//     menu.classList.toggle("menutoggle");
+//     menuup.classList.add("hide");
+//     menudown.classList.remove("hide");
+//   });
+// }
+
+
+
 home.addEventListener("click", () => {
+  location.replace('');
   homeM.classList.remove("hide"); homeA.classList.remove("hide");
   bookmarksM.classList.add("hide"); bookmarksA.classList.add("hide");
   uniM.classList.add("hide"); uniA.classList.add("hide");
   cheatsM.classList.add("hide"); cheatsA.classList.add("hide");
-  menu.classList.toggle("menutoggle");
+  menu.classList.remove("showmenu");
   menuup.classList.add("hide");
   menudown.classList.remove("hide");
+  window.scrollTo(0, 0);
 });
 uni.addEventListener("click", () => {
+  location.replace('#uni');
   homeM.classList.add("hide"); homeA.classList.add("hide");
   bookmarksM.classList.add("hide"); bookmarksA.classList.add("hide");
   uniM.classList.remove("hide"); uniA.classList.remove("hide");
   cheatsM.classList.add("hide"); cheatsA.classList.add("hide");
-  menu.classList.toggle("menutoggle");
+  menu.classList.remove("showmenu");
   menuup.classList.add("hide");
   menudown.classList.remove("hide");
+  window.scrollTo(0, 0);
 });
 bookmarks.addEventListener("click", () => {
+  location.replace('#bookmarks');
   homeM.classList.add("hide"); homeA.classList.add("hide");
   bookmarksM.classList.remove("hide"); bookmarksA.classList.remove("hide");
   uniM.classList.add("hide"); uniA.classList.add("hide");
   cheatsM.classList.add("hide"); cheatsA.classList.add("hide");
-  menu.classList.toggle("menutoggle");
+  menu.classList.remove("showmenu");
   menuup.classList.add("hide");
   menudown.classList.remove("hide");
+  window.scrollTo(0, 0);
 });
 cheats.addEventListener("click", () => {
   homeM.classList.add("hide"); homeA.classList.add("hide");
   bookmarksM.classList.add("hide"); bookmarksA.classList.add("hide");
   uniM.classList.add("hide"); uniA.classList.add("hide");
   cheatsM.classList.remove("hide"); cheatsA.classList.remove("hide");
-  menu.classList.toggle("menutoggle");
+  menu.classList.remove("showmenu");
   menuup.classList.add("hide");
   menudown.classList.remove("hide");
+  window.scrollTo(0, 0);
 });
 
 
