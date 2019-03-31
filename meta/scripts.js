400a08f228d7dac83d435148a4c19fa93562870e
Fania Ra <fania@web.de>
2019-03-31 17:43:15 +0100

    radio button hack menu

--- meta/scripts.js
+++ meta/scripts.js
@@ -10,21 +10,52 @@ if (window.matchMedia("(min-width: 900px)").matches) {
 
   // DESKTOP
 
+  const [...radios] = document.getElementsByName("mainnav");
+  const [...sections] = document.querySelectorAll("[id$='section']");
+  // console.log(sections);
+
+  radios.forEach(i => {
+    i.addEventListener("change", ()=> {
+      console.log("CHANGED", i.id);
+      const l = document.querySelector(".left");
+      const r = document.querySelector(".right");
+      l.classList.add("move");
+      r.classList.add("move");
+
+      sections.forEach(s => s.classList.add("hide"));
+
+      switch (i.id) {
+        case "about_button":
+          about_section.classList.remove("hide");
+          console.log("showing about");
+          break;
+        case "bookmarks_button":
+          interlude_section.classList.remove("hide");
+          console.log("showing interlude");
+          break;
+        default:
+          break;
+      }
+
+      
+
+    });
+  });
+
+
   leftmenu.classList.remove("hide");
   rightmenu.classList.remove("hide");
 
-  about.addEventListener("click", ()=> {
-    const l = document.querySelector(".left");
-    const r = document.querySelector(".right");
-    l.classList.add("move");
-    r.classList.add("move");
-  });
+
+  
+
+
 
 
 } else {
 
   // MOBILE
-  
+
   menuopen.addEventListener("click", () => {
     leftmenu.classList.remove("hide");
     rightmenu.classList.remove("hide");
