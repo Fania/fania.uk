66683f6d997129518734af82f0686ecfe244f449
Fania Ra <fania@web.de>
2019-04-01 20:35:31 +0100

    complete url system

--- meta/scripts.js
+++ meta/scripts.js
@@ -1,3 +1,20 @@
+const pages = ["about", "bookmarks", "cheats", "images"];
+
+// Menu buttons
+window.addEventListener("hashchange", () => {
+  const url = window.location.hash;
+  const page = url.slice(1);
+  if(pages.includes(page)) {
+    const p = page.replace("_section", "");
+    const r = document.getElementById(p + "_button");
+    r.checked = true;
+    document.title = `Fania | ${page}`;
+  } else {
+    const h = document.getElementById("home_button");
+    h.checked = true;
+    document.title = `Fania.eu`;
+  };
+});
 
 
 
@@ -17,21 +34,21 @@ if (window.matchMedia("(min-width: 900px)").matches) {
   radios.forEach(i => {
     i.addEventListener("change", ()=> {
       console.log("CHANGED", i.id);
-      const l = document.querySelector(".left");
-      const r = document.querySelector(".right");
-      l.classList.add("move");
-      r.classList.add("move");
+      // const l = document.querySelector(".left");
+      // const r = document.querySelector(".right");
+      // l.classList.add("move");
+      // r.classList.add("move");
 
-      sections.forEach(s => s.classList.add("hide"));
+      // sections.forEach(s => s.classList.add("hide"));
 
       switch (i.id) {
         case "about_button":
-          about_section.classList.remove("hide");
+          // about_section.classList.remove("hide");
           console.log("showing about");
           break;
         case "bookmarks_button":
-          interlude_section.classList.remove("hide");
-          console.log("showing interlude");
+          // interlude_section.classList.remove("hide");
+          console.log("showing bookmarks");
           break;
         default:
           break;
@@ -57,12 +74,14 @@ if (window.matchMedia("(min-width: 900px)").matches) {
   // MOBILE
 
   menuopen.addEventListener("click", () => {
+    console.log("open menu");
     leftmenu.classList.remove("hide");
     rightmenu.classList.remove("hide");
     menuopen.classList.add("hide");
     menuclose.classList.remove("hide");
   });
   menuclose.addEventListener("click", () => {
+    console.log("close menu");
     leftmenu.classList.add("hide");
     rightmenu.classList.add("hide");
     menuclose.classList.add("hide");
