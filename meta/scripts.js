72cb5c92968978992c533508222a818a99e8ed24
Fania Ra <fania@web.de>
2019-04-03 20:15:20 +0100

    mobile checkbox/radio rewrite

--- meta/scripts.js
+++ meta/scripts.js
@@ -1,4 +1,6 @@
 const pages = ["about", "bookmarks", "cheats", "images"];
+const menu = document.getElementById("menu_button");
+
 
 // Menu buttons
 const loadPage = () => {
@@ -7,88 +9,19 @@ const loadPage = () => {
   if(pages.includes(page)) {
     const p = page.replace("_section", "");
     const r = document.getElementById(p + "_button");
-    r.checked = true;
+    r.checked = true;  // check correct page radio
+    menu.checked = false;  // reset menu
     document.title = `Fania | ${page}`;
   } else {
     const h = document.getElementById("home_button");
-    h.checked = true;
+    h.checked = true;  // check homepage radio
+    menu.checked = false;  // reset menu
     document.title = `Fania.eu`;
   };
 };
 
 window.addEventListener("hashchange", loadPage);
 window.addEventListener("load", loadPage);
-
-
-
-// MOBILE MENU
-if (window.matchMedia("(min-width: 900px)").matches) {
-
-  // DESKTOP
-
-  const [...radios] = document.getElementsByName("mainnav");
-  const [...sections] = document.querySelectorAll("[id$='section']");
-  // console.log(sections);
-
-  radios.forEach(i => {
-    i.addEventListener("change", ()=> {
-      console.log("CHANGED", i.id);
-      // const l = document.querySelector(".left");
-      // const r = document.querySelector(".right");
-      // l.classList.add("move");
-      // r.classList.add("move");
-
-      // sections.forEach(s => s.classList.add("hide"));
-
-      switch (i.id) {
-        case "about_button":
-          // about_section.classList.remove("hide");
-          console.log("showing about");
-          break;
-        case "bookmarks_button":
-          // interlude_section.classList.remove("hide");
-          console.log("showing bookmarks");
-          break;
-        default:
-          break;
-      }
-
-      
-
-    });
-  });
-
-
-  leftmenu.classList.remove("hide");
-  rightmenu.classList.remove("hide");
-
-
-  
-
-
-
-
-} else {
-
-  // MOBILE
-
-  // menuopen.addEventListener("click", () => {
-  //   console.log("open menu");
-  //   leftmenu.classList.remove("hide");
-  //   rightmenu.classList.remove("hide");
-  //   menuopen.classList.add("hide");
-  //   menuclose.classList.remove("hide");
-  // });
-  // menuclose.addEventListener("click", () => {
-  //   console.log("close menu");
-  //   leftmenu.classList.add("hide");
-  //   rightmenu.classList.add("hide");
-  //   menuclose.classList.add("hide");
-  //   menuopen.classList.remove("hide");
-  // });
-
-
-}
 
 
 
