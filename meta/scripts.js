223bdb757b1d3e5e6bda82c31ecbc0746e276895
Fania Ra <fania@web.de>
2020-03-26 13:15:08 +0000

    refactor cheats and add bookmark links and subnav

--- meta/scripts.js
+++ meta/scripts.js
@@ -1,12 +1,30 @@
-const pages = ["about", "bookmarks", "cheats", "images", "cats", "secret"];
+const pages = ["about", "bookmarks", "images", "cats", "secret"];
 const menu = document.getElementById("menu_button");
 
 
 // Menu buttons
 const loadPage = () => {
   const url = window.location.hash;
-  const page = url.slice(1);
-  if(pages.includes(page)) {
+  const re = /#(\w+)\??(\w+)?/;
+  const found = url.match(re);
+  let page = found===null ? "home" : found[1];
+  if(page === "cheats") {
+    const r = document.getElementById("cheats_button");
+    r.checked = true;  // check correct page radio
+    menu.checked = false;  // reset menu
+    document.title = `Fania | ${page}`;
+    let query = "";
+    if (found[2]) {
+      query = found[2];
+      const elem = document.getElementById(query);
+      const mum = elem.parentElement.parentElement;
+      mum.open = true;
+      window.scroll({
+        top: elem.offsetTop - 20,
+        behavior: 'smooth'
+      });
+    }
+  } else if(pages.includes(page)) {
     const p = page.replace("_section", "");
     const r = document.getElementById(p + "_button");
     r.checked = true;  // check correct page radio
@@ -31,15 +49,19 @@ const [...ups] = document.getElementsByClassName("up");
 
 ups.forEach( u => {
   u.addEventListener("click", ()=> {
-    console.log("scroll");
+    // console.log("scroll");
     window.scroll({top: 0, left: 0, behavior: 'smooth'});
   });
 });
 
 
-
-
-
-
-
+// add bookmark links to individual cheats and populate sub nav
+const [...summaries] = document.getElementsByTagName("summary");
+summaries.forEach(item => {
+  const child = item.children[0];
+  child.innerHTML += `<a class="shortcut" href="${location.origin}#cheats?${child.id}">§</a>`;
+  cheatsNav.insertAdjacentHTML("beforeend", `
+    <a href="${location.origin}#cheats?${child.id}">${(child.innerText).slice(0,-1)}</a>
+  `);
+});
 
