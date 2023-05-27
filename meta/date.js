f0a367884b5671087009f797fdca36754338aea3
Fania Everitt <me@fania.eu>
2023-05-27 12:59:52 +0100

    months refer to array of names

--- meta/date.js
+++ meta/date.js
@@ -2,6 +2,9 @@
 // Pataphysical date for footer
 
 // let now = new Date();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 console.log(document.lastModified);
 
 let lastMod = new Date(document.lastModified);
@@ -10,12 +13,12 @@ let curDay = lastMod.getDate();
 let curMonth = lastMod.getMonth();
 let curYear = lastMod.getFullYear();
 
-console.log(`${curDay} ${curMonth} ${curYear}`);
+console.log(`${curDay} ${months[curMonth]} ${curYear}`);
 
 let lastEdited = new Date("27 May 2023");
 // let pnow = new PataphysicalDate();
 let plastEdited = new PataphysicalDate(lastEdited);
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 date.innerHTML = plastEdited;
 date.setAttribute("title", `${lastEdited.getDate()} ${months[lastEdited.getMonth()]} ${lastEdited.getFullYear()} vulg.`);
