cf4437ceb047ca75b153d76582d6f5eda7081ded
Fania Everitt <me@fania.eu>
2023-05-27 12:55:48 +0100

    try date stuff

--- meta/date.js
+++ meta/date.js
@@ -3,6 +3,15 @@
 
 // let now = new Date();
 console.log(document.lastModified);
+
+let lastMod = document.lastModified;
+
+let curDay = lastMod.getDate();
+let curMonth = lastMod.getMonth();
+let curYear = lastMod.getFullYear();
+
+console.log(`${curDay} ${curMonth} ${curYear}`);
+
 let lastEdited = new Date("27 May 2023");
 // let pnow = new PataphysicalDate();
 let plastEdited = new PataphysicalDate(lastEdited);
