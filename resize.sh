92b4cf983411cbbdb725e0fa7724635e4491d7d9
Fania Everitt <me@fania.eu>
2023-05-28 22:52:29 +0100

    create script for generating responsive imgs

---
+++ resize.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# call script by passing it the path to folder for images
+# e.g. 
+# ./resize.sh images/gallery/2003
+
+cd $1
+echo $(pwd)
+
+function imageLoop() {
+  for i in $(pwd)/*; do
+    echo $i
+    # echo "inside imageLoop"
+    if [[ -f $i ]]; then
+      # echo "item is a file"
+      re="(.+)(\.{1}.{4})"
+      if [[ $i =~ $re ]]; then 
+        name20="${BASH_REMATCH[1]}@20x${BASH_REMATCH[2]}"
+        name40="${BASH_REMATCH[1]}@40x${BASH_REMATCH[2]}"
+        name60="${BASH_REMATCH[1]}@60x${BASH_REMATCH[2]}"
+        name80="${BASH_REMATCH[1]}@80x${BASH_REMATCH[2]}"
+        magick $i -resize 20% $name20
+        magick $i -resize 40% $name40
+        magick $i -resize 60% $name60
+        magick $i -resize 80% $name80
+      fi
+    elif [[ -d $i ]]; then
+      break
+    fi
+
+  done
+}
+imageLoop
