7639448cf886c52bb45bf65880c215d24c12f07d
Fania Everitt <me@fania.eu>
2023-06-01 20:40:25 +0100

    turn bool around

--- meta/scripts.js
+++ meta/scripts.js
@@ -84,10 +84,9 @@ summaries.forEach(item => {
 const cheats_filter_Items = document.querySelectorAll('[name="show_hide_cheatsNav"]');
 console.log(cheats_filter_Items);
 
-const prev_cheats_filter_status = Boolean(localStorage.getItem("cheats-filters"));
+const prev_cheats_filter_status = Boolean(!localStorage.getItem("cheats-filters"));
 console.log('prev_cheats_filter_status', prev_cheats_filter_status);
 console.log(typeof prev_cheats_filter_status);
-console.log(typeof Boolean(prev_cheats_filter_status));
 let show_cheatsNav = document.querySelector('#show_cheatsNav');
 let hide_cheatsNav = document.querySelector('#hide_cheatsNav');
 
