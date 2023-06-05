114d236c689c0d1fd8b5cbb3650acc664e965f6a
Fania Everitt <me@fania.eu>
2023-06-05 21:02:46 +0100

    working resize script progress

--- resize.sh
+++ resize.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# set -o rematchpcre
+
 # Call script by passing it the path to folder for images
 # e.g. 
 # ./resize.sh images/gallery/2003
@@ -7,6 +9,7 @@
 # 700px / 1000px / 1200px
 # 43.75em / 62.5em / 75em
 
+# https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html#tag_09_04
 
 # - 3200 x 1800 (QHD+)
 # - 2560×1440 (1440p, QHD)
@@ -28,43 +31,53 @@ cd $1
 echo $(pwd)
 echo "-----------------------------------"
 
+if [[ ! -d 'rspnvs' ]]; then
+  mkdir 'rspnvs'
+fi
+
+
 function resizeLoop() {
   for i in $(pwd)/*; do
     echo $i
     # echo "inside imageLoop"
     if [[ -f $i ]]; then
       # echo "item is a file"
-      re="(.+)(\.{1}.{3,4})"
+      re='([\-a-z_A-Z0-9]+\.[a-z0-9]{3,4})'  # works
+      # if [[ $i =~ ([\-a-z_A-Z0-9]+\.[a-z0-9]{3,4}) ]]; then 
       if [[ $i =~ $re ]]; then 
-      # 512
-        name426="${BASH_REMATCH[1]}@426w${BASH_REMATCH[2]}"
-        name640="${BASH_REMATCH[1]}@640w${BASH_REMATCH[2]}"
-        name720="${BASH_REMATCH[1]}@720w${BASH_REMATCH[2]}"
-        name960="${BASH_REMATCH[1]}@960w${BASH_REMATCH[2]}"
-        name1280="${BASH_REMATCH[1]}@1280w${BASH_REMATCH[2]}"
-        # name80="${BASH_REMATCH[1]}@2048w${BASH_REMATCH[2]}"
+# /Users/fania/Documents/GitHub/fania.uk/images/portfolio/qpoems.webm
+# /Users/fania/Documents/GitHub/fania.uk/images/portfolio/responsive-spiral.avif
+        echo 'test'
+        echo "matches: ${BASH_REMATCH}"
+        # echo "0: ${BASH_REMATCH[0]}"
+        # echo "1: ${BASH_REMATCH[1]}"
+        # echo "2: ${BASH_REMATCH[2]}"
+        name640="rspnvs/${BASH_REMATCH[1]}@640w${BASH_REMATCH[2]}"
+        name960="rspnvs/${BASH_REMATCH[1]}@960w${BASH_REMATCH[2]}"
+        name1280="rspnvs/${BASH_REMATCH[1]}@1280w${BASH_REMATCH[2]}"
+        name1920="rspnvs/${BASH_REMATCH[1]}@1920w${BASH_REMATCH[2]}"
+        name2560="rspnvs/${BASH_REMATCH[1]}@2560w${BASH_REMATCH[2]}"
+
         if [[ "${BASH_REMATCH[2]}" == ".mp4" || 
               "${BASH_REMATCH[2]}" == ".webm" ]]; then 
           # VIDEOS
-          ffmpeg -i $i -vf scale=426:-2 $name426 -nostats -loglevel 0
-          ffmpeg -i $i -vf scale=640:-2  $name640 -nostats -loglevel 0
-          ffmpeg -i $i -vf scale=720:-2 $name720 -nostats -loglevel 0
-          ffmpeg -i $i -vf scale=960:-2 $name960 -nostats -loglevel 0
+          ffmpeg -i $i -vf scale=640:-2  $name640  -nostats -loglevel 0
+          ffmpeg -i $i -vf scale=960:-2  $name960  -nostats -loglevel 0
           ffmpeg -i $i -vf scale=1280:-2 $name1280 -nostats -loglevel 0
+          ffmpeg -i $i -vf scale=1920:-2 $name1920 -nostats -loglevel 0
+          ffmpeg -i $i -vf scale=2560:-2 $name2560 -nostats -loglevel 0
           # ffmpeg -i $i -vf scale=iw/5:-2    $name512 -nostats -loglevel 0
-          # ffmpeg -i $i -vf scale=iw/2.5:-2  $name768 -nostats -loglevel 0
-          # ffmpeg -i $i -vf scale=iw/1.68:-2 $name1024 -nostats -loglevel 0
         else
           # IMAGES
           # magick $i -resize 20% $name512
-          # magick $i -resize 40% $name768
-          # magick $i -resize 60% $name1024
-          magick $i -resize 426x240\> $name426
-          magick $i -resize 640x360\> $name640
-          magick $i -resize 720x480\> $name720
-          magick $i -resize 960x540\> $name960
-          magick $i -resize 1280x720\> $name1280
+          magick $i -resize 640x360\>   $name640
+          magick $i -resize 960x540\>   $name960
+          magick $i -resize 1280x720\>  $name1280
+          magick $i -resize 1920x1080\> $name1920
+          magick $i -resize 2560x1440\> $name2560
         fi
+      else 
+        echo 'regex didnt work'
       fi
     elif [[ -d $i ]]; then
       break
