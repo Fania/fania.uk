05431a2643b59491b074298ac5d1356fd8c9e833
Fania Everitt <me@fania.eu>
2023-12-17 13:27:01 +0000

    make sw changes

---
+++ sw-BACKUP.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// https://developer.chrome.com/docs/workbox/caching-strategies-overview/
+// audio, document, font, image, manifest, object, script, style, video
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+
+const cacheName = 'fania-v1.0.0';
+
+// ===================================================================
+// Stale-while-revalidate
+self.addEventListener('fetch', (event) => {
+  // console.log(`${event.request.destination}: ${event.request.url}`);
+  if (event.request.destination === 'image' ||
+      event.request.destination === 'audio' ||
+      event.request.destination === 'font' ||
+      event.request.destination === 'manifest' ||
+      event.request.destination === 'object' ||
+      event.request.destination === 'script' ||
+      event.request.destination === 'style' ||
+      event.request.destination === 'video'
+     ) {
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          const fullurl = new URL(event.request.url);
+          const urlOri = fullurl.origin;
+          // only adds new resources in, not updates out-of-date ones?
+          // this line avoids things like "chrome-extension://xyz"
+          if((urlOri.startsWith('http')) || (urlOri.startsWith('https'))){
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+  } else {
+    return;
+  }
+});
+// ===================================================================
+// Cache first, falling back to network
+// self.addEventListener('fetch', (event) => {
+//   console.log(`${event.request.destination}: ${event.request.url}`);
+//   if (event.request.destination === 'image' ||
+//       event.request.destination === 'audio' ||
+//       event.request.destination === 'font' ||
+//       event.request.destination === 'manifest' ||
+//       event.request.destination === 'object' ||
+//       event.request.destination === 'script' ||
+//       event.request.destination === 'style' ||
+//       event.request.destination === 'video'
+//      ) {
+//     event.respondWith(caches.open(cacheName).then((cache) => {
+//       return cache.match(event.request.url).then((cachedResponse) => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
+//         // Otherwise, hit the network
+//         return fetch(event.request).then((fetchedResponse) => {
+//           // Add the network response to the cache for later visits
+//           cache.put(event.request, fetchedResponse.clone());
+//           return fetchedResponse;
+//         });
+//       });
+//     }));
+//   } else {
+//     return;
+//   }
+// });
+// ===================================================================
+
--- sw.js
+++ sw.js
@@ -1,71 +1,114 @@
 'use strict';
 
-// https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-// audio, document, font, image, manifest, object, script, style, video
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
-const cacheName = 'fania-v1.0.0';
+const cacheName = 'fania-v1.0.1';
 
-// ===================================================================
-// Stale-while-revalidate
-self.addEventListener('fetch', (event) => {
-  // console.log(`${event.request.destination}: ${event.request.url}`);
-  if (event.request.destination === 'image' ||
-      event.request.destination === 'audio' ||
-      event.request.destination === 'font' ||
-      event.request.destination === 'manifest' ||
-      event.request.destination === 'object' ||
-      event.request.destination === 'script' ||
-      event.request.destination === 'style' ||
-      event.request.destination === 'video'
-     ) {
-    event.respondWith(caches.open(cacheName).then((cache) => {
-      return cache.match(event.request).then((cachedResponse) => {
-        const fetchedResponse = fetch(event.request).then((networkResponse) => {
-          const fullurl = new URL(event.request.url);
-          const urlOri = fullurl.origin;
-          // only adds new resources in, not updates out-of-date ones?
-          // this line avoids things like "chrome-extension://xyz"
-          if((urlOri.startsWith('http')) || (urlOri.startsWith('https'))){
-            cache.put(event.request, networkResponse.clone());
-          }
-          return networkResponse;
-        });
-        return cachedResponse || fetchedResponse;
-      });
-    }));
-  } else {
-    return;
-  }
+const deleteCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+  const cacheKeepList = ["fania-v1.0.0"];
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  await Promise.all(cachesToDelete.map(deleteCache));
+};
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(deleteOldCaches());
 });
-// ===================================================================
-// Cache first, falling back to network
-// self.addEventListener('fetch', (event) => {
-//   console.log(`${event.request.destination}: ${event.request.url}`);
-//   if (event.request.destination === 'image' ||
-//       event.request.destination === 'audio' ||
-//       event.request.destination === 'font' ||
-//       event.request.destination === 'manifest' ||
-//       event.request.destination === 'object' ||
-//       event.request.destination === 'script' ||
-//       event.request.destination === 'style' ||
-//       event.request.destination === 'video'
-//      ) {
-//     event.respondWith(caches.open(cacheName).then((cache) => {
-//       return cache.match(event.request.url).then((cachedResponse) => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
-//         // Otherwise, hit the network
-//         return fetch(event.request).then((fetchedResponse) => {
-//           // Add the network response to the cache for later visits
-//           cache.put(event.request, fetchedResponse.clone());
-//           return fetchedResponse;
-//         });
-//       });
-//     }));
-//   } else {
-//     return;
-//   }
-// });
-// ===================================================================
+
+// --------------------------------------------------------------------------
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open(cacheName);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to use (and cache) the preloaded response, if it's there
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info("using preload response", preloadResponse);
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
+// Enable navigation preload
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/haskell.html",
+      "/now/index.html",
+      "/hedgehog/index.html",
+      "/starlings/index.html",
+      "/meta/style.css",
+      "/meta/gallery.css",
+      "/meta/scripts.js",
+      "/meta/date.js",
+      "/meta/gallery.js",
+      "/meta/pataphysicalDate.min.js",
+      "/favicon.ico",
+      "/site.webmanifest"
+    ]),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+      fallbackUrl: "/images/femalecodertocat.png",
+    }),
+  );
+});
+
 
